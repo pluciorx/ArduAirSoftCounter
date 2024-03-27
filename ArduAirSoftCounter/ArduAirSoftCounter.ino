dfde4ce537746b50b08158caa777e0ae5aafb542
Marcin Pluta <pluciorx@gmail.com>
2024-03-27 15:20:14 +0100

    clean libraries

--- ArduAirSoftCounter/ArduAirSoftCounter.ino
+++ ArduAirSoftCounter/ArduAirSoftCounter.ino
@@ -1,8 +1,5 @@
 
 #include <Adafruit_Debounce.h>
-#include <Button2.h>
-#include <Hardware.h>
-#include <LiquidCrystal.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // I2C address 0x27, 20 column and 4 rows
